7eccc0d4671399cc3f51ba85d9d81e24ab2666a4
Weerawut <weerawut@live.com>
2024-12-17 10:18:20 +0700

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Weerawut Chaiyasomboon on 17/12/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
