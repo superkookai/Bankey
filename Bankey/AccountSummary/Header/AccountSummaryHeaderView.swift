59bca60f15c0970a8af27940487434a77cae56ca
Weerawut <weerawut@live.com>
2024-12-23 18:16:57 +0700

    Add Button shaking at LoginViewController and ShakeyBell at AccountSummaryHeaderView

--- Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -11,31 +11,45 @@ class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
     
+    let shakeyBellView = ShakeyBellView()
+    
     override init(frame: CGRect) {
-            super.init(frame: frame)
-            commonInit()
-        }
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
         
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            commonInit()
-        }
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         
-        override var intrinsicContentSize: CGSize {
-            return CGSize(width: UIView.noIntrinsicMetric, height: 144)
-        }
+        setupShakeyBell()
+    }
+    
+    private func setupShakeyBell() {
+        shakeyBellView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(shakeyBellView)
         
-        private func commonInit() {
-            let bundle = Bundle(for: AccountSummaryHeaderView.self)
-            bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
-            addSubview(contentView)
-            contentView.backgroundColor = appColor
-            
-            contentView.translatesAutoresizingMaskIntoConstraints = false
-            contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        }
+        NSLayoutConstraint.activate([
+            shakeyBellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shakeyBellView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
 
